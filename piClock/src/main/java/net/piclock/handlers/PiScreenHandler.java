e84cb255a75e46f32e5be836ef20ec7623880872
alex20111 <boudale@yahoo.com>
2020-02-14 10:49:40 -0500

    Update to LUX program

--- piClock/src/main/java/net/piclock/handlers/PiScreenHandler.java
+++ piClock/src/main/java/net/piclock/handlers/PiScreenHandler.java
@@ -14,6 +14,8 @@ import net.piclock.enums.Light;
 
 public class PiScreenHandler {
 
+	private String ldrFile = "home/pi/native/TSL2561/tslReading.rdr";
+	private Path  ldrFilePath = null;
 	
 	public void setScreenBrightness(Light light) {
 
@@ -32,19 +34,28 @@ public class PiScreenHandler {
 	}
 	
 
-	public int getVisibleLight() throws ExecuteException, IOException {
+	public int getVisibleLight() throws ExecuteException, IOException {		
+
+		new Thread(new Runnable(){
+
+		@Override
+		public void run() {
+			Exec exe = new Exec();
+			exe.addCommand("./native/2591").addCommand(ldrFile).addCommand("1").addCommand("0");
+			exe.run();		
+		}			
+		}).start();
 		
-//		Exec exe = new Exec();
-//		exe.addCommand("./native/2591");
-//		
-//		exe.timeout(5000);
-//		exe.run();
-//		
-//		String ret = exe.getOutput();
-//		
-//		System.out.println("Info: " + ret);
+	//read from file
+		if (ldrFilePath == null){
+			ldrFilePath = Paths.get(fileLoc);
+		}		
 		
-		System.out.println("Getting light");
+		List<String> r = Files.readAllLines(f);
+		
+		String split[] = r.get(0).split(",");	
+				
+		System.out.println("Getting light - Visible : " + split[2] );
 		return 176;
 	}
 
@@ -100,9 +111,7 @@ public class PiScreenHandler {
 			}
 			
 			
-		}
-		
-		
+		}		
 		
 
 
@@ -111,70 +120,3 @@ public class PiScreenHandler {
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import com.pi4j.io.i2c.I2CBus;
-//import com.pi4j.io.i2c.I2CDevice;
-//import com.pi4j.io.i2c.I2CFactory;
-//import java.io.IOException;
-//import java.text.DecimalFormat;
-//import java.text.NumberFormat;
-//
-//public class TSL2561
-//{
-//	public static void main(String args[]) throws Exception
-//	{
-//		// Create I2C bus
-//		I2CBus Bus = I2CFactory.getInstance(I2CBus.BUS_1);
-//		// Get I2C device, TSL2561 I2C address is 0x39(57)
-//		I2CDevice device = Bus.getDevice(0x39);
-//
-//		// Select control register
-//		// Power ON mode
-//		device.write(0x00 | 0x80, (byte)0x03);
-//		// Select timing register
-//		// Nominal integration time = 402ms
-//		device.write(0x01 | 0x80, (byte)0x02);
-//		Thread.sleep(500);
-//
-//		// Read 4 bytes of data
-//		// ch0 lsb, ch0 msb, ch1 lsb, ch1 msb
-//		byte[] data=new byte[4];
-//		device.read(0x0C | 0x80, data, 0, 4);
-//
-//		// Convert the data
-//		double ch0 = ((data[1] & 0xFF)* 256 + (data[0] & 0xFF));
-//		double ch1 = ((data[3] & 0xFF)* 256 + (data[2] & 0xFF));
-//
-//		// Output data to screen
-//		System.out.printf("Full Spectrum(IR + Visible) : %.2f lux %n", ch0);
-//		System.out.printf("Infrared Value : %.2f lux %n", ch1);
-//		System.out.printf("Visible Value : %.2f lux %n", (ch0 - ch1));
-//	}
-//}
